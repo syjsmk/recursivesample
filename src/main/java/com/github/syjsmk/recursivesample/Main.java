acd965e8b73d2509bffeb08e64beb0219ea543ee
syjsmk <syjsmk@naver.com>
2016-02-19 01:51:33 +0900

    Update Main.java

--- src/main/java/com/github/syjsmk/recursivesample/Main.java
+++ src/main/java/com/github/syjsmk/recursivesample/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Fibonacci fibonacci = new Fibonacci();
 
-        int n = 10;
+        int n = 30;
         for (int i = 0; i < n; i++)
             System.out.println(i + ": " + fibonacci.calculation(i));
     }
